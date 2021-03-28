979c6f8f10dbdec2c2199e16594e29608ce36594
Wahyu Irwansyah <wahyuiwangsyah@gmail.com>
2021-03-28 07:30:34 +0700

    add permission

--- app/styles/StatusBar.js
+++ app/styles/StatusBar.js
@@ -1,6 +1,6 @@
-import { StyleSheet, StatusBar, Platform } from 'react-native';
-import { IphoneXHelper } from '../util';
-import { Theme } from '.';
+import {StyleSheet, StatusBar, Platform} from 'react-native';
+import {IphoneXHelper} from '../util';
+import Theme from './Theme';
 
 const StatusBarHeight = IphoneXHelper.getStatusBarHeight();
 
